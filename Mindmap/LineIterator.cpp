8ba655db665156ddb55a905749f0e77a7f2594df
dohveloper <dohveloper@gmail.com>
2018-02-03 13:44:43 +0900

    Iterator 추가중

---
+++ Mindmap/LineIterator.cpp
@@ -0,0 +1,43 @@
+﻿#include "LineIterator.h"
+#include "Branch.h"
+#include "Line.h"
+#include "Topic.h"
+
+LineIterator::LineIterator(Branch* branch) {
+	this->branch = branch;
+	current = 0;
+}
+
+void LineIterator::First()
+{
+	Long length;
+	Long i = 0;
+	Shape *current;
+
+	length = branch->GetLength();
+
+	while (i < length) {
+		current = this->branch->GetAt(i);
+		if (typeid(*current) != typeid(Topic)) {
+			this->current = current;
+		}
+		if (typeid(*current) != typeid(Branch)) {
+			
+		i++;
+	}
+
+}
+
+void LineIterator::Next()
+{
+}
+
+bool LineIterator::IsDone() const
+{
+	return false;
+}
+
+Shape * LineIterator::CurrentItem() const
+{
+	return this->current;
+}
---
+++ Mindmap/LineIterator.h
@@ -0,0 +1,19 @@
+﻿#ifndef _LINEITERATOR_H
+#define _LINEITERATOR_H_
+#include "Iterator.h"
+class Branch;
+
+class LineIterator :public Iterator{
+public:
+	LineIterator(Branch* branch);
+	virtual void First();
+	virtual void Next();
+	virtual bool IsDone() const;
+	virtual Shape* CurrentItem() const;
+
+private:
+	Branch *branch;
+	Shape *current;
+};
+
+#endif //_LINEITERATOR_H_
