9671d1fa42c0d72ec46f02bc0b133230cd3f0490
dohveloper <dohveloper@gmail.com>
2018-02-08 16:17:55 +0900

    기타 오류 수정

--- Mindmap/DrawingVisitor.cpp
+++ Mindmap/DrawingVisitor.cpp
@@ -3,18 +3,16 @@
 #include "PageForm.h"
 #include "DrawLines.h"
 #include "DrawTopics.h"
-//#include "DrawMarks.h"
+#include "DrawMarks.h"
 
 DrawingVisitor::DrawingVisitor() {
-
 }
 
-DrawingVisitor::DrawingVisitor(CPaintDC *dc){
+DrawingVisitor::DrawingVisitor(CPaintDC *dc) {
 	this->dc = dc;
 }
 
 DrawingVisitor::~DrawingVisitor() {
-
 }
 
 void DrawingVisitor::VisitBranch(Branch *branch)
@@ -25,6 +23,6 @@ void DrawingVisitor::VisitBranch(Branch *branch)
 	DrawTopics drawTopics(branch, dc);
 	drawTopics.Traverse();
 
-	//DrawMarks drawMarks(branch, dc);
-	//DrawMarks.Traverse();
+	DrawMarks drawMarks(branch, dc);
+	drawMarks.Traverse();
 }
