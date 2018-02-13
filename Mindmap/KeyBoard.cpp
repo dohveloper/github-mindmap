9bc3b823cc2c6cc56a6213f27ce914700e6e04ab
naw1441 <naw1441@naver.com>
2018-02-13 12:11:17 +0900

    키보드 완성
    딜리트키 클래스 분리 완성. 다른 키들은 키보드 액션에 객체선언하고 키클래스 작성하면 됨

--- Mindmap/KeyBoard.cpp
+++ Mindmap/KeyBoard.cpp
@@ -18,16 +18,60 @@ void KeyBoard::KeyDown(PageForm *pageForm, UINT nChar, UINT nRepCnt, UINT nFlags
 	switch (nChar)
 	{
 	case VK_DELETE:
+	{
 		DeleteKey deleteKey;
-		//this->keyAction = new DeleteKey;
-		this->keyAction->KeyPress(pageForm);
+		deleteKey.KeyPress(pageForm);
+		break;
+	}
+	case VK_RETURN://엔터
+	{
+		break;
+	}
+	case VK_SPACE://스페이스
+	{
+		break;
+	}
+	case VK_ESCAPE://ESC
+	{
+		break;
+	}
+	case VK_TAB://탭
+	{
+		break;
+	}
+	case VK_BACK://백스페이스
+	{
+		break;
+	}
+	case VK_LEFT://왼쪽방향
+	{
+		break;
+	}
+	case VK_RIGHT://오른쪽방향
+	{
+		break;
+	}
+	case VK_UP://위쪽방향
+	{
+		break;
+	}
+	case VK_DOWN://아래쪽방향
+	{
 		break;
 	}
 	/*
-	if (this->scrollMove != NULL)
+	case MK_CONTROL + 0x43:
 	{
-	delete this->scrollMove;
-	this->scrollMove = NULL;
+		break;
+	}
+	case 0x56:
+	{
+		break;
 	}
 	*/
+	//0x56
+	default:
+		break;
+	}
+
 }
