155fc57bbdc3841192be98f7bf215d026a7ac955
dohveloper <dohveloper@gmail.com>
2018-02-13 12:18:43 +0900

    MoveVisitor 메인

--- Mindmap/MoveVisitor.cpp
+++ Mindmap/MoveVisitor.cpp
@@ -62,3 +62,34 @@ void MoveVisitor::VisitBranch(Branch *branch)
 		i++;
 	}
 }
+
+#include <iostream>
+#include "Mark.h"
+#include "Topic.h"
+#include "Line.h"
+int main(int argc, char *argv[]) {
+	Topic topic(1000, 1000, 1000, 1000, "토픽");
+	Line line(10, 10, 10, 10, "라인");
+	Mark mark(1, 1, 1, 1, "마크");
+	Branch branch;
+
+	branch.Add(&topic);
+	branch.Add(&line);
+	branch.Add(&mark);
+
+	//이동전 값 확인
+	cout << " Topic :  " << topic.GetX() << " " << topic.GetY() << " " << topic.GetWidth() << " " << topic.GetHeight() << " " << topic.GetContent() << " " << endl;
+	cout << " Line :  " << line.GetX() << " " << line.GetY() << " " << line.GetWidth() << " " << line.GetHeight() << " " << line.GetContent() << " " << endl;
+	cout << " Mark :  " << mark.GetX() << " " << mark.GetY() << " " << mark.GetWidth() << " " << mark.GetHeight() << " " << mark.GetContent() << " " << endl;
+
+	//이동
+	MoveVisitor visitor(9, 9);
+	branch.Accept(visitor);
+
+	//이동후 값 확인
+	cout << " Topic :  " << topic.GetX() << " " << topic.GetY() << " " << topic.GetWidth() << " " << topic.GetHeight() << " " << topic.GetContent() << " " << endl;
+	cout << " Line :  " << line.GetX() << " " << line.GetY() << " " << line.GetWidth() << " " << line.GetHeight() << " " << line.GetContent() << " " << endl;
+	cout << " Mark :  " << mark.GetX() << " " << mark.GetY() << " " << mark.GetWidth() << " " << mark.GetHeight() << " " << mark.GetContent() << " " << endl;
+
+	return 0;
+}
