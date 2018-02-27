3cf7857d09ce3bf2c4645a47b1f86bff280f078c
dohveloper <dohveloper@gmail.com>
2018-02-27 16:54:09 +0900

    DeleteKey KeyAction상속

--- Mindmap/DeleteKey.h
+++ Mindmap/DeleteKey.h
@@ -2,15 +2,15 @@
 
 #ifndef _DELETEKEY_H
 #define _DELETEKEY_H
+#include "KeyAction.h"
 
 class PageForm;
-class DeleteKey
+class DeleteKey :public KeyAction
 {
 public:
 	DeleteKey();
 	virtual ~DeleteKey();
 	void KeyPress(PageForm *pageForm);
-
 };
 
 #endif // _DELETEKEY_H
